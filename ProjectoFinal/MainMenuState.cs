72d3b98de1fd5805897c596c2eaba681f4a6d840
Carlos Roque <carlosroque198@gmail.com>
2019-10-04 11:53:35 -0400

    small changes

--- ProjectoFinal/MainMenuState.cs
+++ ProjectoFinal/MainMenuState.cs
@@ -8,7 +8,35 @@ namespace ProjectoFinal
     {
         public void Enter()
         {
-            Console.WriteLine("wee");
+            while (true)
+            {
+                Console.WriteLine("¿Qué desea hacer?");
+                Console.WriteLine("1. Ver propio perfil");
+                Console.WriteLine("2. Buscar un perfil existente");
+                Console.WriteLine("3. Crear un chat privado");
+                Console.WriteLine("4. Ver Lista de circulos existente");
+                Console.WriteLine("5. Salir de la aplicación");
+                Console.Write("R: ");
+                byte input;
+
+                if (byte.TryParse(Console.ReadLine(), out input))
+                {
+                    switch (input)
+                    {
+                        case 1:
+
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            continue;
+                    }
+                }
+            }
         }
     }
 }
