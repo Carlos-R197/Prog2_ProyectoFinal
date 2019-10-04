033dec0fcb4a75daec2e070735e3b09e4772a1b8
Carlos Roque <carlosroque198@gmail.com>
2019-10-04 10:54:38 -0400

    Main menu state added

---
+++ ProjectoFinal/MainMenuState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectoFinal
+{
+    public class MainMenuState : IState
+    {
+        public void Enter()
+        {
+            Console.WriteLine("wee");
+        }
+    }
+}
--- ProjectoFinal/Program.cs
+++ ProjectoFinal/Program.cs
@@ -6,7 +6,8 @@ namespace ProjectoFinal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StateMachine appState = new StateMachine();
+            appState.ChangeState(new MainMenuState());
         }
     }
 }
