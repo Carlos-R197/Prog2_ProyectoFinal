3728fe4edbcda2130061a4cb99280c3ff538a602
MARIANO JOSE VASQUEZ RIVAS <1087284@est.intec.edu.do>
2019-10-04 13:37:25 -0400

    Creacion de las clases Chat y Mensaje

---
+++ ProjectoFinal/Chat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectoFinal
+{
+    class Chat
+    {
+        public Perfil Perfil1 { get; set; }
+        public Perfil Perfil2 { get; set; }
+        public List<Mensaje> Conversacion { get; set; }
+
+        public void AddMensaje(Mensaje mensaje)
+        {
+            Conversacion.Add(mensaje);
+        }
+        public Chat(Perfil perfil1, Perfil perfil2)
+        {
+            this.Perfil1 = perfil1;
+            this.Perfil2 = perfil2;
+        }
+    }
+}
---
+++ ProjectoFinal/Mensaje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectoFinal
+{
+    class Mensaje
+    {
+        public DateTime HoraEnvio { get; set; }
+        public Perfil perfil { get; set; }
+        public string SMS { get; set; }
+
+    }
+}
