32d78443d4937fef4966f5066be3fd74f261f04a
Carlos Roque <carlosroque198@gmail.com>
2019-10-04 10:23:18 -0400

    State machine added

---
+++ ProjectoFinal/StateMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectoFinal
+{
+    public class StateMachine
+    {
+        private IState currentState;
+        private IState firstState;
+
+        public void GoBackToMainMenu()
+        {
+            ChangeState(firstState);
+        }
+
+        public void ChangeState(IState newState)
+        {
+            if (currentState == null)
+                firstState = newState;
+
+            currentState = newState;
+            currentState.Enter();
+        }
+    }
+}
