8f7136681ca688039b01dce5de2273fd56790e15
MARIANO JOSE VASQUEZ RIVAS <1087284@est.intec.edu.do>
2019-10-07 11:22:12 -0400

    quite  basura

--- ProjectoFinal/Program.cs
+++ ProjectoFinal/Program.cs
@@ -12,28 +12,6 @@ namespace ProjectoFinal
             StateMachine appState = new StateMachine();
 
             appState.ChangeState(new InicioState());
-            /*
-            Console.WriteLine("Escriba un nombre");
-            string nombre = Console.ReadLine();
-            DataTable table = new DataTable();
-            using (MySqlCommand cmd = new MySqlCommand("SELECT nombre FROM perfiles_registrados WHERE nombre='"+nombre+"'", SQLManager.conexion))
-            {
-                MySqlDataAdapter dap = new MySqlDataAdapter(cmd);
-                SQLManager.AbrirConexion();
-                dap.Fill(table);
-                SQLManager.CerrarConexion();
-            }
-            Console.WriteLine(table.Rows.Count);
-            foreach (DataRow datarow in table.Rows)
-            {
-                foreach (var item in datarow.ItemArray)
-                {
-
-                    Console.WriteLine("{0} \n", item);
-
-                }
-            }
-            */
 
         }
     }
